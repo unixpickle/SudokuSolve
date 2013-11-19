71c082b22bd40a475a00f59d74ce15c661374639
Alex Nichol <alexnichol@comcast.net>
2013-11-19 16:01:57 -0500

    Initial Commit

---
+++ SudokuSolve/ANAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ANAppDelegate.h
+//  SudokuSolve
+//
+//  Created by Alex Nichol on 11/19/13.
+//  Copyright (c) 2013 Alex Nichol. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ANAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ SudokuSolve/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SudokuSolve
+//
+//  Created by Alex Nichol on 11/19/13.
+//  Copyright (c) 2013 Alex Nichol. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
